c17871dbe955fd994055067b9393b624d69a3e35
hyuan <yhaomin2007@gmail.com>
2026-09-03 11:42:57 -0500

    Add eulerEuler.cpp to APP_SOURCES so the plugin actually compiles
    Without this, eulerEuler.cpp/.hpp were dead code from CMake's
    perspective -- not part of libnekrs at all, so even a full CI build
    would never have caught a compile error in them.

--- cmake/nekrs.cmake
+++ cmake/nekrs.cmake
@@ -9,6 +9,7 @@ set(APP_SOURCES
     ${APP_SOURCES_DIR}/nrs/constantFlowRate.cpp
     ${APP_SOURCES_DIR}/nrs/plugins/RANSktau.cpp
     ${APP_SOURCES_DIR}/nrs/plugins/lowMach.cpp
+    ${APP_SOURCES_DIR}/nrs/plugins/eulerEuler.cpp
     ${APP_SOURCES_DIR}/nrs/postProcessing/registerPostProcessingKernels.cpp
     ${APP_SOURCES_DIR}/nrs/postProcessing/strainRotationRate.cpp
     ${APP_SOURCES_DIR}/nrs/postProcessing/aeroForces.cpp
